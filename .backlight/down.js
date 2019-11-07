4e2f7e24fa0e42c968a35625d32c343e07b04a0e
Timothy Ha <timothy@Chopin.local>
2019-11-06 22:07:28 -0800

    Initial commit.

---
+++ .backlight/down.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var HID = require('node-hid');
+fs = require('fs');
+
+var devices = HID.devices();
+device = new HID.HID(4292,33742);
+
+fs.readFile('/Users/timothy/.backlight/level', 'utf8', function (err,data) {
+	if (err) {
+		return console.log(err);
+	}
+	data = parseInt(data);
+	if (data > 0)
+		data = data - 24;
+	device.write([6, data]);
+	fs.writeFile('/Users/timothy/.backlight/level', data, (err) => {
+		if (err) throw err;
+	});
+});
---
+++ .backlight/max.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var HID = require('node-hid');
+fs = require('fs');
+
+var devices = HID.devices();
+device = new HID.HID(4292,33742);
+
+fs.readFile('/Users/timothy/.backlight/level', 'utf8', function (err,data) {
+	if (err) {
+		return console.log(err);
+	}
+	data = 240;
+	device.write([6, data]);
+	fs.writeFile('/Users/timothy/.backlight/level', data, (err) => {
+		if (err) throw err;
+	});
+});
