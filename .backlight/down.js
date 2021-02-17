f66a3adf3caf8ae8878ee092ea47ef5bfe3c0211
Timothy Ha <timothy@chen-ha.com>
2021-02-17 12:48:04 -0800

    updated for big sur

--- .backlight/down.js
+++ .backlight/down.js
@@ -7,15 +7,10 @@ device = new HID.HID(4292,33742);
 
 path = '/Users/timothy/.backlight/level';
 
-fs.readFile(path, 'utf8', function (err,data) {
-	if (err) {
-		return console.log(err);
-	}
+fs.readFile(path, 'utf8', function (err, data) {
+	if (err) return console.log(err);
 	data = parseInt(data);
-	if (data > 0)
-		data -= 16;
+	if (data > 0) data -= 16;
 	device.write([6, data]);
-	fs.writeFile(path, data, (err) => {
-		if (err) throw err;
-	});
+	fs.writeFile(path, data.toString(), (err) => {if (err) throw err;});
 });
